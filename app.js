e663d9ef43c1fe2203768cd6096821311f787361
brielalmeida <gabrielalmeida.dev@outlook.com>
2023-11-09 17:49:55 -0300

    final

--- app.js
+++ app.js
@@ -12,18 +12,6 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-//firebase
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app')
-const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore')
-var admin = require("firebase-admin");
-var serviceAccount = require("./correios-dc355-firebase-adminsdk-15m0z-1af7d436cd.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-})
-const db = getFirestore();
-
-
-
 //tradutor
 const translate = require('translator-for-you')
 translate('Hello, my name is mark, i am a 20 year old boy who needs therapy', 'pt').then(result => console.log(result)).catch(err => console.log(err));
